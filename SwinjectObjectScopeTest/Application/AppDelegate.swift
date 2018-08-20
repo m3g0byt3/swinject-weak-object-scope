44849ebf71f8ba8d23fe2e50345a2d49299ac420
m3g0byt3 <m3g0byt3@gmail.com>
2018-08-20 12:57:28 +0300

    Make AppDelegate version-agnostic

--- SwinjectObjectScopeTest/Application/AppDelegate.swift
+++ SwinjectObjectScopeTest/Application/AppDelegate.swift
@@ -13,10 +13,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    #if swift(>=4.2)
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         return true
     }
+    #else
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+    #endif
 }
